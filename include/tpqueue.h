933b81c676373d8f95d5610447075e8a62f0397d
Kirill Kashkirov <124998794+Kashkirov@users.noreply.github.com>
2023-04-15 23:45:07 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,40 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+	int head;
+	int tail;
+	int fullness;
+	T arr[size];
+	void QueueSort() {
+		if (this->fullness < 2) {
+			return;
+		}
+		for (int i = head + fullness; i > head; i--) {
+			if (arr[(i - 1) % size].pr <= arr[(i) % size].pr) {
+				T temp = arr[(i - 1) % size];
+				arr[(i - 1) % size] = arr[(i) % size];
+				arr[(i) % size] = temp;
+			}
+		}
+	}
+ public:
+	TPQueue() :head(0), tail(0), fullness(0) {}
+	void push(const T& value) {
+		if (fullness == size) {
+			throw std::string("Already full Queue");
+			return;
+		}
+		arr[(tail++) % size] = value;
+		QueueSort();
+		fullness++;
+		return;
+	}
+	const T& pop() {
+		fullness--;
+		SYM val = arr[head % size];
+		return arr[(head++) % size];
+	}
 };
 
 struct SYM {
